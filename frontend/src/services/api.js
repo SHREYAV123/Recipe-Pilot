0dd5feeb903a28610717bd873d3c2626c3559828
Shreya Sahu <shreyasahu9696@gmail.com>
2026-07-17 22:00:37 +0530

    add a backend lunk using env file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,18 +1,23 @@
 import axios from "axios";
 
+// Backend URL
+console.log(import.meta.env.VITE_API_URL);
 const API = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
+// Generate Recipe
 export const generateRecipe = async (data) => {
   try {
-    const response = await API.post("/recipe", data);
+    const response = await API.post("/recipe/generate", data);
+
     return response.data;
   } catch (error) {
     console.error(error);
+
     throw error;
   }
 };
